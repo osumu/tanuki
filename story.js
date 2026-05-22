1053a3033b2ce90f7a1c1886a65fce8f5508a63d
OSUMU <tsuna21c@outlook.jp>
2026-05-22 20:18:11 +0200

    Add files via upload

--- story.js
+++ story.js
@@ -1,330 +1,119 @@
 function getScenario() {
-
-  return [
-    {
-      type: "message1",
-      image: "test.png",
-      name: "解説",
-      text: "ぼくはたぬき。どこにでもいるふつうのたぬき。\n今日は友達のひよこ、ねこと山の中をさんさくしている。"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "解説",
-      text: "しばらく歩いていると、ふいに パーン パパーン と音がした。"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "たぬき、ひよこ、ねこ",
-      text: "「？」「？」「？」\nみんなで首をかしげた。"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "ひよこ",
-      text: "「なんだろう？」"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "ねこ",
-      text: "「おばけ！？」"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "たぬき",
-      text: "「音がしたほうに行ってみる？」"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "解説",
-      text: "みんな怖そうだけれど、行くことにした。"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "解説",
-      text: "しばらく歩くと、小屋が見えた。"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "たぬき",
-      text: "（あれは...？）"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "解説",
-      text: "何かが動いた！\n\n\nまた動いた。"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "解説",
-      text: "好奇心がおさえられなくて、近づいてみた。"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "たぬき、ひよこ、ねこ",
-      text: "（あっ！）\n\n誰か小屋から出てきた！\nねこが何かにとびついた！"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "？？",
-      text: "パーン！！"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "たぬき",
-      text: "「うっ！」"
-    },
-    {
-      type: "message1",
-      image: "test.png",
-      name: "解説",
-      text: "続く"
-    },
-    {
-      type: "choice",
-      image: "test.png",
-      name: "解説",
-      text: "選択が出た！",
-      choices: [
+    return [
+        { label: "prologue" }, { type: "message1", image: "test.png", name: "解説", text: "ぼくはたぬき。どこにでもいるふつうのたぬき。\n今日は友達のひよこ、ねこと山の中をさんさくしている。" }, { type: "message1", image: "test.png", name: "解説", text: "しばらく歩いていると、ふいに パーン パパーン と音がした。" }, { type: "message1", image: "test.png", name: "たぬき、ひよこ、ねこ", text: "「？」「？」「？」\nみんなで首をかしげた。" }, { type: "message1", image: "test.png", name: "ひよこ", text: "「なんだろう？」" }, { type: "message1", image: "test.png", name: "ねこ", text: "「おばけ！？」" }, { type: "message1", image: "test.png", name: "たぬき", text: "「音がしたほうに行ってみる？」" }, { type: "message1", image: "test.png", name: "解説", text: "みんな怖そうだけれど、行くことにした。" }, { type: "message1", image: "test.png", name: "解説", text: "しばらく歩くと、小屋が見えた。" }, { type: "message1", image: "test.png", name: "たぬき", text: "（あれは...？）" }, { type: "message1", image: "test.png", name: "解説", text: "何かが動いた！\n\nまた動いた。" }, { type: "message1", image: "test.png", name: "解説", text: "好奇心がおさえられなくて、近づいてみた。" }, { type: "message1", image: "test.png", name: "たぬき、ひよこ、ねこ", text: "（あっ！）\n\n誰か小屋から出てきた！\nねこが何かにとびついた！" }, { type: "message1", image: "test.png", name: "？？", text: "パーン！！" }, { type: "message1", image: "black.png", name: "たぬき", text: "「うっ！」" }, { type: "choice", image: "black.png", name: "タイトルへ", text: "", choices: [{ text: "タイトルへ", jump: "htmlTitle" }] },
+        { label: "startGame" }, { type: "message1", image: "black.png", name: "たぬき", text: "「んん？」「あっ！」\n（ここはどこ？ぼくはどうしたの？）\n（ひよことねこはどこ？）\n\n あ 思い出した！" },
         {
-          text: "話を聞く",
-          jump: "talkRoute"
+            type: "choice",
+            image: "test.png",
+            name: "たぬき",
+            text: "「呪ってやろうかな？ でも一生友達だよって言ったし...」",
+            choices: [
+                {
+                    text: "呪う",
+                    jump: "curse"
+                },
+                {
+                    text: "呪わない",
+                    jump: "ncurse"
+                }
+            ]
         },
         {
-          text: "立ち去る",
-          jump: "leaveRoute"
-        }
-      ]
-    },
-
-    {
-      label: "talkRoute"
-    },
-
-    {
-      type: "message1",
-
-      image: "test.png",
-
-      name: "少女",
-
-      text: "森では最近、魔物が増えているんです..."
-    },
-
-    {
-      type: "message2",
-
-      image: "test.png",
-
-      name: "勇者",
-
-      text: "なるほど。気をつけよう。"
-    },
-
-    {
-      type: "choice",
-
-      image: "test.png",
-
-      choices: [
-
-        {
-          text: "森へ向かう",
-
-          jump: "forestRoute"
+            type: "choice",
+            image: "test.png",
+            name: "最後の選択へ",
+            choices: [
+                {
+                    text: "最後の選択へ",
+                    jump: "saigo"
+                }
+            ]
         },
 
         {
-          text: "村へ戻る",
-
-          jump: "villageRoute"
-        }
-
-      ]
-    },
-
-    /*
-      森ルート
-    */
-    {
-      label: "forestRoute"
-    },
-
-    {
-      type: "message1",
-
-      image: "test.png",
-
-      name: "SYSTEM",
-
-      text: "魔物が現れた！"
-    },
-
-    {
-      type: "choice",
-
-      image: "test.png",
-
-      choices: [
-
+            label: "curse"
+        },
         {
-          text: "戦う",
-
-          jump: "battleEnd"
+            type: "message1",
+            image: "test.png",
+            name: "たぬき",
+            text: "呪うことにした。"
         },
 
         {
-          text: "逃げる",
-
-          jump: "runEnd"
-        }
-
-      ]
-    },
-
-    /*
-      村ルート
-    */
-    {
-      label: "villageRoute"
-    },
-
-    {
-      type: "message2",
-
-      image: "test.png",
-
-      name: "SYSTEM",
-
-      text: "あなたは村へ戻った。"
-    },
-
-    {
-      type: "choice",
-
-      image: "test.png",
-
-      choices: [
+            type: "message2",
+            image: "test.png",
+            name: "??",
+            text: "何か入力..."
+        },
 
         {
-          text: "タイトルへ",
-
-          jump: "title"
-        }
-
-      ]
-    },
-
-    /*
-      戦闘終了
-    */
-    {
-      label: "battleEnd"
-    },
-
-    {
-      type: "message2",
-
-      image: "test.png",
-
-      name: "SYSTEM",
-
-      text: "魔物を倒した！"
-    },
-
-    {
-      type: "choice",
-
-      image: "test.png",
-
-      choices: [
+            label: "ncurse"
+        },
+        {
+            type: "message1",
+            image: "test.png",
+            name: "たぬき",
+            text: "呪わないことにした。"
+        },
+        {
+            type: "choice",
+            image: "test.png",
+            name: "よし！やっぱり呪うぞ！",
+            choices: [
+                {
+                    text: "呪う",
+                    jump: "curse"
+                }
+            ]
+        },
+        { label: "saigo" },
+        {
+            type: "choice",
+            image: "test.png",
+            name: "最後の選択",
+            text: "?",
+            choices: [
+                {
+                    text: "呪う",
+                    jump: "curse2"
+                },
+                {
+                    text: "呪わない",
+                    jump: "nocurse"
+                }
+            ]
+        },
 
         {
-          text: "タイトルへ",
-
-          jump: "title"
-        }
-
-      ]
-    },
-
-    /*
-      逃走終了
-    */
-    {
-      label: "runEnd"
-    },
-
-    {
-      type: "message2",
-
-      image: "test.png",
-
-      name: "SYSTEM",
-
-      text: "あなたは逃げ出した。"
-    },
-
-    {
-      type: "choice",
-
-      image: "test.png",
-
-      choices: [
+            label: "nocurse"
+        },
+        {
+            type: "message1",
+            image: "test.png",
+            name: "?",
+            text: "何か入力..."
+        },
+        {
+            label: "curse2"
+        },
+        {
+            type: "message2",
+            image: "test.png",
+            name: "?",
+            text: "呪うことにした"
+        },
 
         {
-          text: "タイトルへ",
-
-          jump: "title"
-        }
-
-      ]
-    },
-
-    /*
-      立ち去るルート
-    */
-    {
-      label: "leaveRoute"
-    },
-
-    {
-      type: "message2",
-
-      image: "test.png",
-
-      name: "SYSTEM",
-
-      text: "あなたは村を後にした。"
-    },
-
-    {
-      type: "choice",
-
-      image: "test.png",
-
-      choices: [
-
-        {
-          text: "タイトルへ",
-
-          jump: "title"
-        }
-
-      ]
-    }
-
-  ];
-
+            type: "choice",
+            image: "test.png",
+            name: "選択",
+            choices: [
+                {
+                    text: "タイトルへ",
+                    jump: "title"
+                }
+            ]
+        },
+    ];
 }
